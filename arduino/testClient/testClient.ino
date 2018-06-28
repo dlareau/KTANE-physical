9721092e52077a26f8f4219a6c5d6477849c6e26
Dillon Lareau <flybye22@gmail.com>
2018-06-27 22:52:42 -0400

    made real test programs

--- arduino/testClient/testClient.ino
+++ arduino/testClient/testClient.ino
@@ -1,11 +1,27 @@
 #include "dserial.h"
+#include <NeoICSerial.h>
+NeoICSerial serial_port;
+DSerialClient client(&serial_port, 2);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial.println("AltSoftSerial Test Begin");
+  serial_port.begin(9600);
+  serial_port.println("Hello World");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  char c;
+  char data[2] = " ";
+  char out_message[MAX_MSG_LEN];
 
+  if (Serial.available()) {
+    c = Serial.read();
+    data[0] = c;
+    client.sendData(data);
+  }
+  if (client.getData(out_message)) {
+    Serial.print(out_message);
+  }
+  client.doSerial();
 }
--- arduino/testMaster/testMaster.ino
+++ arduino/testMaster/testMaster.ino
@@ -1,11 +1,27 @@
 #include "dserial.h"
+#include <NeoICSerial.h>
+NeoICSerial serial_port;
+DSerialMaster master(&serial_port);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial.println("AltSoftSerial Test Begin");
+  serial_port.begin(9600);
+  serial_port.println("Hello World");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  char c;
+  char data[2] = " ";
+  char out_message[MAX_MSG_LEN];
 
+  if (Serial.available()) {
+    c = Serial.read();
+    data[0] = c;
+    master.sendData(2, data);
+  }
+  if (master.getData(out_message)) {
+    Serial.print(out_message);
+  }
+  master.doSerial();
 }
