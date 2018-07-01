0285cd8047591b1f57b2e2bddfb541b5f7f73efa
Dillon Lareau <flybye22@gmail.com>
2018-06-30 20:32:45 -0400

    updated testModule

--- arduino/testModule/testModule.ino
+++ arduino/testModule/testModule.ino
@@ -8,19 +8,43 @@ NeoICSerial serial_port;
 DSerialClient client(serial_port, MY_ADDRESS);
 KTANEModule module(client);
 
+int button_state = 0;
+int last_button_state = 0;
+
 void setup() {
   serial_port.begin(9600);
+  Serial.begin(9600);
+
+  // BEGIN TEMP COMMANDS
+  pinMode(2, INPUT);
+  // END TEMP COMMANDS
+
+  Serial.print("B");
   while(!module.getConfig()){
     module.interpretData();
   }
+  Serial.print("A");
+  //randomizeModule();
+  module.sendReady();
+  Serial.print("S");
 }
 
 void loop() {
-  int is_solved = 0;
   module.interpretData();
+
+  // TEMP
+  button_state = digitalRead(2);
+  if (button_state != last_button_state && button_state) {
+    last_button_state = button_state;
+    module.sendStrike();
+    Serial.print("E");
+    delay(5);
+  }
+
   /*
-  if(!is_solved){
+  if(!module.is_solved){
     checkInputs();
+    they_solved_it = checkIfSolved();
     updateOutputs();
     if(they_solved_it) {
       module.sendSolve();
