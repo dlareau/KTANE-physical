54e9140d829bb956af4403f43280126fe3fec0f5
Dillon Lareau <flybye22@gmail.com>
2018-07-17 13:42:41 -0400

    Changes to Simon Says needed to run on hardware

--- arduino/simonSaysModule/simonSays.ino
+++ arduino/simonSaysModule/simonSays.ino
@@ -7,12 +7,13 @@ DSerialClient client(serial_port, MY_ADDRESS);
 KTANEModule module(client, 3, 4);
 
 #define MAX_NUM_STAGES 5
-#define RED 0
-#define YELLOW 1
-#define GREEN 2
-#define BLUE 3
-#define BASE_LED_PIN 13
-#define BASE_BUTTON_PIN 9
+#define RED 1
+#define YELLOW 2
+#define GREEN 3
+#define BLUE 4
+
+int led_pins[4] = {15, 10, 11, 5};
+int button_pins[4] = {14, 7, 12, 6};
 
 int stage;
 int num_stages = random(3, MAX_NUM_STAGES + 1);
@@ -34,71 +35,78 @@ void update_lights(){
   static unsigned long old_millis = 0;
   static int light_stage = 0;
 
-
-  if(light_stage >= ((stage + 1)*2) && (millis() - old_millis > 700)) {
-    old_millis = millis();
-    light_stage = 0;
-  } else if((light_stage % 2 == 0) && (millis() - old_millis > 300)) {
-    digitalWrite(BASE_LED_PIN + stage_colors[light_stage/2], HIGH);
-    old_millis = millis();
-    light_stage++;
-  } else if((light_stage % 2 == 1) && (millis() - old_millis > 700)) {
-    digitalWrite(BASE_LED_PIN + stage_colors[light_stage/2], LOW);
-    old_millis = millis();
-    light_stage++;
+  if(light_stage >= ((stage + 1)*2)) {
+    if(millis() - old_millis > 700) {
+      old_millis = millis();
+      light_stage = 0;
+    }
+  } else {
+    if((light_stage % 2 == 0) && (millis() - old_millis > 300)) {
+      digitalWrite(led_pins[stage_colors[light_stage/2]], HIGH);
+      old_millis = millis();
+      light_stage++;
+    } else if((light_stage % 2 == 1) && (millis() - old_millis > 700)) {
+      digitalWrite(led_pins[stage_colors[light_stage/2]], LOW);
+      old_millis = millis();
+      light_stage++;
+    }
   }
-
 }
 
 void setup() {
   serial_port.begin(19200);
   Serial.begin(19200);
 
-  pinMode(BASE_BUTTON_PIN, INPUT);
-  pinMode(BASE_BUTTON_PIN + 1, INPUT);
-  pinMode(BASE_BUTTON_PIN + 2, INPUT);
-  pinMode(BASE_BUTTON_PIN + 3, INPUT);
-  pinMode(BASE_LED_PIN, OUTPUT);
-  pinMode(BASE_LED_PIN + 1, OUTPUT);
-  pinMode(BASE_LED_PIN + 2, OUTPUT);
-  pinMode(BASE_LED_PIN + 3, OUTPUT);
+  randomSeed(analogRead(A5));
 
-  while(!module.getConfig()){
-    module.interpretData();
-  }
+  pinMode(button_pins[0], INPUT);
+  pinMode(button_pins[1], INPUT);
+  pinMode(button_pins[2], INPUT);
+  pinMode(button_pins[3], INPUT);
+  pinMode(led_pins[0], OUTPUT);
+  pinMode(led_pins[1], OUTPUT);
+  pinMode(led_pins[2], OUTPUT);
+  pinMode(led_pins[3], OUTPUT);
+
+  // while(!module.getConfig()){
+  //   module.interpretData();
+  // }
 
   for(int i = 0; i < num_stages; i++) {
     stage_colors[i] = random(0, 4);
   }
   stage = 0;
 
-  module.sendReady();
+  // module.sendReady();
 }
 
 void loop() {
-  module.interpretData();
+  // module.interpretData();
   int button_pressed = 0;
 
   if(!module.is_solved){
-    if(digitalRead(BASE_BUTTON_PIN)) {
+    if(digitalRead(button_pins[0])) {
       button_pressed = RED;
-    } else if(digitalRead(BASE_BUTTON_PIN + 1)) {
+    } else if(digitalRead(button_pins[1])) {
       button_pressed = YELLOW;
-    } else if(digitalRead(BASE_BUTTON_PIN + 2)) {
+    } else if(digitalRead(button_pins[2])) {
       button_pressed = GREEN;
-    } else if(digitalRead(BASE_BUTTON_PIN + 3)) {
+    } else if(digitalRead(button_pins[3])) {
       button_pressed = BLUE;
     }
 
-    int vowel = module.serialContainsVowel();
-    int strikes = module.getNumStrikes();
+
+    int vowel = 0;//module.serialContainsVowel();
+    int strikes = 0;// module.getNumStrikes();
     int light_color = stage_colors[stage];
+    update_lights();
+    // I don't check button sequences, only last button, need to check all.
     if(button_pressed == mapping[vowel][strikes][light_color]) {
       stage++;
       if(stage == num_stages){
         module.win();
       }
-    } else {
+    } else if(button_pressed != 0) {
       module.strike();
     }
   }
