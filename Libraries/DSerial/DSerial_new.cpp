54a15424d96dfe4eda3812063cd071e8d71d06be
Dillon Lareau <flybye22@gmail.com>
2019-12-16 15:58:35 -0500

    start of dserial rewrite

---
+++ Libraries/DSerial/DSerial_new.cpp
@@ -0,0 +1,127 @@
+/** @file dserial.cpp
+ *  @brief The DSerial library implementation
+ *
+ *  @author Dillon Lareau (dlareau)
+ */
+
+#include "Arduino.h"
+#include "DSerial.h"
+#include <string.h>
+#include "stringQueue.h"
+
+/** @brief Reads a packet from the specified stream if one is available
+ *
+ *  in: stream
+ *  out: packet, client, len, retval
+ *
+ */
+int readPacket(Stream &s, char *buffer){
+
+}
+
+/** @brief Writes a packet to the specified stream.
+ *
+ *  in: steam, packet, client, len
+ *  out: retval
+ *
+ */
+int sendPacket(Stream &s, char *message){
+
+}
+
+/** @brief Creates a new DSerialMaster object
+ *
+ *  @param port The underlying stream object used for communication.
+
+ *  @return A new initialized DSerialMaster object
+ */
+DSerialMaster::DSerialMaster(Stream &port):_stream(port){
+
+}
+
+/** @brief sends a data string to the specified client.
+ *
+ *  Internally, this function enqueues the data to be written when convenient.
+ *  This function fails when there is not enough memory to enqueue the message
+ *  or when the queue is full.
+ *
+ *  @param client_id  The ID of the client to write to
+ *  @param data       The data to write to the given client
+ *  @return A status code indicating success or failure
+ */
+int DSerialMaster::sendData(uint8_t client_id, char *data){
+
+}
+
+/** @brief Retrieve data if there is any to get
+ *
+ *  @param buffer A string to populate with the possible data
+ *  @return The ID of the client that sent the message, 0 if no data.
+ */
+int DSerialMaster::getData(char *buffer){
+
+}
+
+/** @brief runs a client search
+ *
+ *  A client search consists of pinging each client address between 1 and
+ *  MAX_CLIENTS. If the client responds, then it gets put in our array.
+ *
+ *  @return The number of clients found
+ */
+int DSerialMaster::identifyClients() {
+
+}
+
+/** @brief gets the client array and number of clients
+ *
+ *  If the clients argument is NULL, the number of clients will still be
+ *  returned, but it will not attempt to populate the array.
+ *
+ *  @param clients  a pointer to memory of at least MAX_CLIENTS size to put
+                      the the clients into
+ *  @return The number of clients found
+ */
+int DSerialMaster::getClients(uint8_t *clients){
+
+}
+
+int DSerialMaster::doSerial(){
+
+}
+
+/** @brief Creates a new DSerialClient object
+ *
+ *  @param port The underlying stream object used for communication.
+
+ *  @return A new initialized DSerialClient object
+ */
+DSerialClient::DSerialClient(Stream &port, uint8_t client_number):_stream(port){
+
+}
+
+/** @brief sends a data string to the master.
+ *
+ *  Internally, this function enqueues the data to be written when convenient.
+ *  This function fails when there is not enough memory to enqueue the message
+ *  or when the queue is full.
+ *
+ *  @param data The data to write to the given client
+ *  @return A status code indicating success or failure
+ */
+int DSerialClient::sendData(char *data){
+
+}
+
+/** @brief Retrieve data if there is any to get
+ *
+ *  @param buffer A string to populate with the possible data
+ *  @return A status code indicating whether data was retrieved
+ */
+int DSerialClient::getData(char *buffer){
+
+}
+
+int DSerialClient::doSerial(){
+
+}
---
+++ Libraries/DSerial/DSerial_new.h
@@ -0,0 +1,121 @@
+/** @file dserial.h
+ *  @brief Headers and definitions for the DSerial Library
+ *
+ *  The DSerial library adds a layer-3/4 interface intended to be used on top
+ *  of a serial UART multi-drop bus. DSerial adds addressing and message 
+ *  acknowledgment in a master/client configuration. Clients are polled for
+ *  data. 
+ *
+ *  The library is divided into Master and Client classes. In order for constant
+ *  data flow, all communicating parties should call "doSerial" often. Functions
+ *  in the DSerial library do not block and all but getClients should be
+ *  execute relatively quickly.
+ *
+ *  Currently, only bytes with values between 1 and 255 inclusive are allowed
+ *  support for sending 0's is hopefully coming soon.
+ *
+ *  Definitions:
+ *    - Packet: Data in the form of {START}{MESSAGE}{PARITY}{END}
+ *      - Currently, the first byte of the message is the client address
+ *    - Valid addresses for clients are between 1 and MAX_CLIENTS
+ *      - MAX_CLIENTS can be at most 126.
+ *
+ *  The overall interaction method with this library should be through the 
+ *  sendData and getData methods on the master and client objects. Unlike the
+ *  notion of a message used internally, these strings do not contain the id
+ *  of the intended recipient.
+ *
+ *  (Internally however, a "message" always contains the destination client id
+ *   as the first byte)
+ *
+ *  At because it is impossible to know which client a corrupted packet was
+ *  intended for, clients to not NAK corrupt packets, instead the transaction
+ *  will just time out and the master node will retry the transaction. This 
+ *  takes longer, but because the whole library is non-blocking it's fine.
+ *
+ *  Future improvements:
+ *    - Switch to all length based binary processing to allow nulls
+ *    - Have the client address be broken out into the packet datatype.
+ *
+ *  Current transaction structure:
+ *    Client -> Master:
+ *      1 M: {READ}
+ *      2 C: {DATA}
+ *      3 M: {ACK}
+ *      4 C: {ACK}
+ *    
+ *    Client -> Master (If no data):
+ *      1 M: {READ}
+ *      2 C: {ACK}
+ *    
+ *    Master -> Client:
+ *      1 M: {WRITE}{DATA}
+ *      2 C: {ACK}
+ *
+ *  @author Dillon Lareau (dlareau)
+ */
+
+#pragma once
+#include "Arduino.h"
+#include "stringQueue.h"
+
+// Control characters
+// All of the form 0x80 + (most appropriate ascii character)
+#define ACK (char)0x86
+#define NAK (char)0x95
+#define START (char)0x82
+#define END (char)0x83
+#define WRITE (char)0xD7
+#define READ (char)0xD2
+#define NO_DATA (char)0xB0
+#define PING (char)0xB1
+#define ESC (char)0x9B
+
+#define TIMEOUT 50
+#define MAX_CLIENTS 16
+#define MAX_MSG_LEN 16
+#define MAX_MASTER_QUEUE_SIZE 40
+#define MAX_CLIENT_QUEUE_SIZE 20
+#define MAX_RETRIES 3
+
+#define MASTER_WAITING 0
+#define MASTER_SENT 1
+#define MASTER_ACK 2
+#define CLIENT_WAITING 0
+#define CLIENT_SENT 1
+
+int readPacket(Stream *s, char *buffer, int len);
+int sendPacket(Stream *s, char *message, int len);
+
+class DSerialMaster {
+  public:
+    DSerialMaster(Stream &port);
+    int sendData(uint8_t client_id, char *data, int len);
+    int getData(char *buffer);
+    int doSerial();
+    int identifyClients();
+    int getClients(uint8_t *clients);
+
+  private:
+    Stream   &_stream;
+    uint8_t   _state;
+    stringQueue_t _in_messages;
+    stringQueue_t _out_messages;
+    uint8_t   _num_clients;
+    uint8_t   _clients[MAX_CLIENTS];
+};
+
+class DSerialClient {
+  public:
+    DSerialClient(Stream &port, uint8_t client_number);
+    int sendData(char *data, int len);
+    int getData(char *buffer);
+    int doSerial();
+
+  private:
+    Stream   &_stream;
+    uint8_t   _state;
+    stringQueue_t _in_messages;
+    stringQueue_t _out_messages;
+    uint8_t   _client_number;
+};
